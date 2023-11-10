bbac4b6eb3aeb8f3c421cefd8ccbc706aa3ef7fe
Marchinka90 <martin_marchev@abv.bg>
2023-11-10 12:16:40 +0200

    Initial commit of the project

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+   return (
+      <div>
+         <h1>Home Page</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium eius expedita eaque quasi ratione labore eveniet. Ipsa reprehenderit amet nisi repellendus deserunt magni iste odit, alias sit doloribus? Expedita, repudiandae!</p>
+      </div>
+   )
+}
+
+export default HomePage
